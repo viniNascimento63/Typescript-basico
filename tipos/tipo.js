af51748853185a8ddc0ec963c882eb741856fd3a
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-21 14:14:08 -0300

    Create tipo.js

---
+++ tipos/tipo.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+* No TS, o tipo de uma variável é inferido
+* na sua inicialização.
+*/
+// string
+let nome = 'Pedro';
+console.log(nome);
+// numbers
+let idade = 17;
+console.log(idade);
+idade = 17.3;
+console.log(idade);
+// boolean
+let possuiHobbies = true;
+console.log(possuiHobbies);
