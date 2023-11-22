14973798122e593677add32ba98aedd641d14c94
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-22 11:53:21 -0300

    Update tipo.js

--- tipos/tipo.js
+++ tipos/tipo.js
@@ -14,3 +14,36 @@ console.log(idade);
 // boolean
 let possuiHobbies = true;
 console.log(possuiHobbies);
+// Tipos explícitos
+let minhaIdade;
+minhaIdade = 22;
+console.log(typeof (minhaIdade));
+// Arrays
+let hobbies = ['Esportes', 'Música', 'Leitura'];
+console.log(hobbies[1]);
+hobbies[3] = 34;
+console.log(hobbies);
+// Tuplas 
+let endereco = ["Rua banana", 342, ""];
+console.log(endereco);
+endereco = ["Rua Limão", 984, "Abacaxi B1"];
+console.log(endereco);
+// enums
+var Cor;
+(function (Cor) {
+    Cor[Cor["Azul"] = 0] = "Azul";
+    Cor[Cor["Vermelho"] = 100] = "Vermelho";
+    Cor[Cor["Roxo"] = 10] = "Roxo";
+    Cor[Cor["Laranja"] = 11] = "Laranja";
+    Cor[Cor["Amarelo"] = 100] = "Amarelo"; // 100
+})(Cor || (Cor = {}));
+let minhaCor = Cor.Azul;
+console.log(minhaCor);
+console.log(Cor.Vermelho);
+console.log(Cor.Amarelo, Cor.Vermelho);
+console.log(Cor.Roxo, Cor.Laranja);
+// any
+let carro = "BMW";
+console.log(carro);
+carro = { Marca: "Mercêdes-Bens", Ano: 2022 };
+console.log(carro);
